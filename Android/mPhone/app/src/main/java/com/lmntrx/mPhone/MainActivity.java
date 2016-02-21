22cdaa20f2fe30a1c748b44ce03af4357a2bc720
Livin <livinmathew99@gmail.com>
2016-02-21 12:20:30 +0530

    Added a basic timer

--- Android/mPhone/app/src/main/java/com/lmntrx/mPhone/MainActivity.java
+++ Android/mPhone/app/src/main/java/com/lmntrx/mPhone/MainActivity.java
@@ -3,6 +3,7 @@ package com.lmntrx.mPhone;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.view.animation.Animation;
@@ -10,28 +11,33 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        final TextView launchDate=(TextView)findViewById(R.id.Date);
+
+        final TextView launchDate = (TextView) findViewById(R.id.Date);
+        final Animation fadeIn;
+        final Animation fadeOut;
         launchDate.animate().alpha(1f).setDuration(3000);
-        final Animation animation1;
-        final Animation animation2;
-        animation2= new AlphaAnimation(1.0f, 0.05f);
-        animation1 = new AlphaAnimation(0.05f, 1.0f);
-        animation1.setDuration(500);
-        animation1.setStartOffset(0);
+        fadeOut = new AlphaAnimation(1.0f, 0.05f);
+        fadeIn = new AlphaAnimation(0.05f, 1.0f);
+        fadeIn.setDuration(500);
+        fadeIn.setStartOffset(0);
 
-        //animation1 AnimationListener
-        animation1.setAnimationListener(new Animation.AnimationListener(){
+        //fadeIn AnimationListener
+        fadeIn.setAnimationListener(new Animation.AnimationListener() {
 
             @Override
             public void onAnimationEnd(Animation arg0) {
-                // start animation2 when animation1 ends (continue)
-                launchDate.startAnimation(animation2);
+                // start fadeOut when fadeIn ends (continue)
+                launchDate.startAnimation(fadeOut);
             }
 
             @Override
@@ -48,16 +54,16 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        animation2.setDuration(500);
-        animation2.setStartOffset(0);
+        fadeOut.setDuration(500);
+        fadeOut.setStartOffset(0);
 
-        //animation2 AnimationListener
-        animation2.setAnimationListener(new Animation.AnimationListener(){
+        //fadeOut AnimationListener
+        fadeOut.setAnimationListener(new Animation.AnimationListener() {
 
             @Override
             public void onAnimationEnd(Animation arg0) {
-                // start animation1 when animation2 ends (repeat)
-                launchDate.startAnimation(animation1);
+                // start fadeIn when fadeOut ends (repeat)
+                launchDate.startAnimation(fadeIn);
             }
 
             @Override
@@ -74,15 +80,14 @@ public class MainActivity extends AppCompatActivity {
 
         });
 
-        launchDate.startAnimation(animation1);
-        ImageView lMango=(ImageView)findViewById(R.id.leftMango);
-        ImageView rMango=(ImageView)findViewById(R.id.rightMango);
-        final ImageView text=(ImageView)findViewById(R.id.mPhoneText);
-        final RelativeLayout comingSoonLayout=(RelativeLayout)findViewById(R.id.comingSoonLayout);
-        final RelativeLayout launcherLayout=(RelativeLayout)findViewById(R.id.launcherLayout);
+        launchDate.startAnimation(fadeIn);
+        ImageView lMango = (ImageView) findViewById(R.id.leftMango);
+        ImageView rMango = (ImageView) findViewById(R.id.rightMango);
+        final ImageView text = (ImageView) findViewById(R.id.mPhoneText);
+        final RelativeLayout comingSoonLayout = (RelativeLayout) findViewById(R.id.comingSoonLayout);
+        final RelativeLayout launcherLayout = (RelativeLayout) findViewById(R.id.launcherLayout);
         lMango.setTranslationX(-2000f);
         rMango.setTranslationX(2000f);
-        //  text.setTranslationY(-6000f);
         lMango.animate().translationXBy(2000f).setDuration(1000);
         rMango.animate().translationXBy(-2000f).setDuration(2000);
         new CountDownTimer(5000, 1000) {
@@ -99,6 +104,35 @@ public class MainActivity extends AppCompatActivity {
                 launcherLayout.setVisibility(View.INVISIBLE);
             }
         }.start();
+
+        String launchDateAndTimeS = "2016-02-29 07:00:00";
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        try {
+            Date launchDateAndTime = dateFormat.parse(launchDateAndTimeS);
+            System.out.println(launchDateAndTime);
+            Date currentDate = new Date();
+            long diff = launchDateAndTime.getTime() - currentDate.getTime();
+            new CountDownTimer(diff, 1000) {
+                @Override
+                public void onTick(long diff) {
+
+                    long seconds = diff / 1000;
+                    long minutes = seconds / 60;
+                    long hours = minutes / 60;
+                    long days = hours / 24;
+                    ((TextView) findViewById(R.id.Date_Timer)).setText(days + "d:" + hours % 24 + "h:" + minutes % 60 + "m:" + seconds % 60 + "s");
+                }
+
+                @Override
+                public void onFinish() {
+
+                }
+            }.start();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
     }
 }
 
