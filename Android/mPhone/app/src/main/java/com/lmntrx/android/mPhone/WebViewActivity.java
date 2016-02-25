fc5f89f765bd14d3effd1d4bd1e429c0be392206
Livin <livinmathew99@gmail.com>
2016-02-25 15:46:00 +0530

    So many Changes..Can't List out :D

--- Android/mPhone/app/src/main/java/com/lmntrx/android/mPhone/WebViewActivity.java
+++ Android/mPhone/app/src/main/java/com/lmntrx/android/mPhone/WebViewActivity.java
@@ -4,17 +4,20 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.ActivityInfo;
+import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.http.SslError;
 import android.os.CountDownTimer;
-import android.os.SystemClock;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
-import android.view.Window;
 import android.webkit.SslErrorHandler;
 import android.webkit.WebChromeClient;
 import android.webkit.WebResourceError;
@@ -28,6 +31,12 @@ import android.widget.ProgressBar;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import me.itangqi.waveloadingview.WaveLoadingView;
+
+/**
+ * GO THROUGH ALL TODOs BEFORE FINALISING APP
+ */
+
 public class WebViewActivity extends AppCompatActivity {
 
 
@@ -35,9 +44,13 @@ public class WebViewActivity extends AppCompatActivity {
 
     ProgressBar progressBar;
 
-    public final String LOG_TAG="mPhoneStore";
+    WaveLoadingView waveLoadingView;
 
-    private final String url = "http://www.lmntrx.com/", domain = "lmntrx-com";  //Replace with "http://shop.mphone.in/" later
+    private UrlCache urlCache = null;
+
+    public final String LOG_TAG = "mPhoneStore";
+
+    private final String url = "http://shop.mphone.in/", domain = "shop-mphone-in";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
@@ -49,62 +62,28 @@ public class WebViewActivity extends AppCompatActivity {
         try {
             //Hiding ActionBar
             getSupportActionBar().hide();
-        }catch (NullPointerException e){
-            Log.e(LOG_TAG,e.getMessage()+" ");
+        } catch (NullPointerException e) {
+            Log.e(LOG_TAG, e.getMessage() + " ");
         }
 
+        waveLoadingView = (WaveLoadingView) findViewById(R.id.waveLoadingView);
 
         //To stop orientation change during splash screen
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_NOSENSOR);
 
 
         //Declaring View Objects
-        ImageView mangoLeft=(ImageView)findViewById(R.id.lmango);
-        ImageView mangoRight=(ImageView)findViewById(R.id.rmango);
+        ImageView mangoLeft = (ImageView) findViewById(R.id.lmango);
+        ImageView mangoRight = (ImageView) findViewById(R.id.rmango);
         final ImageView mangoText = (ImageView) findViewById(R.id.mPhoneText);
-        final TextView lmntrxText=(TextView) findViewById(R.id.lmntrxTxt);
-
-        //Animation code
-        mangoLeft.setTranslationX(-2000);
-        mangoRight.setTranslationX(2000);
-        mangoLeft.animate().translationXBy(2000).setDuration(1500);
-        mangoRight.animate().translationXBy(-2000).setDuration(2500);
-        new CountDownTimer(3000,1000) {
-            @Override
-            public void onTick(long millisUntilFinished) {
-
-            }
-
-            @Override
-            public void onFinish() {
-                mangoText.animate().alpha(1f).setDuration(600);
-                lmntrxText.animate().alpha(1f).setDuration(600);
-            }
-        }.start();
-        //END OF ANIMATION
-
-
-        // SHOWS SPLASH SCREEN AND HIDES IT AFTER GIVEN TIME
-        final RelativeLayout splashLayout=(RelativeLayout) findViewById(R.id.splashLayout);
-
-        splashLayout.setVisibility(View.VISIBLE);
-        splashLayout.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                getSupportActionBar().show();
-                splashLayout.setVisibility(View.GONE);
-                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
-            }
-
-
-        }, 6000); // CHANGE THIS VALUE TO ADJUST SPLASH SCREEN DURATION
-
+        final TextView lmntrxText = (TextView) findViewById(R.id.lmntrxTxt);
+        final RelativeLayout splashLayout = (RelativeLayout) findViewById(R.id.splashLayout);
 
         //WebView Code
         //WebView Elements
         webview = (WebView) findViewById(R.id.webView);
         progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
+        progressBar.getProgressDrawable().setColorFilter(Color.RED, PorterDuff.Mode.SRC_IN);
         //Only required while loading a url
         progressBar.setVisibility(View.GONE);
 
@@ -127,9 +106,13 @@ public class WebViewActivity extends AppCompatActivity {
                 }
 
                 progressBar.setProgress(progress);
+                if (waveLoadingView.getVisibility() == View.VISIBLE)
+                    waveLoadingView.setProgressValue(progress);
 
                 if (progress == 100) {
                     progressBar.setVisibility(View.GONE);
+                    if (waveLoadingView.getVisibility() == View.VISIBLE)
+                        waveLoadingView.setVisibility(View.GONE);
                 }
             }
         });
@@ -138,7 +121,56 @@ public class WebViewActivity extends AppCompatActivity {
         webview.setWebViewClient(new MyWebViewClient(this));
 
         //loading url
-        webview.loadUrl(url);
+        if (isNetworkAvailable()) {
+            webview.loadUrl(url);
+            waveLoadingView.setVisibility(View.VISIBLE);
+        } else {
+            findViewById(R.id.error_msg_layout).setVisibility(View.VISIBLE);
+            webview.setVisibility(View.GONE);
+
+        }
+        //Animation code
+        mangoLeft.setTranslationX(-2000);
+        mangoRight.setTranslationX(2000);
+        mangoLeft.animate().translationXBy(2000).setDuration(1500);
+        mangoRight.animate().translationXBy(-2000).setDuration(2500);
+        new CountDownTimer(3000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+                mangoText.animate().alpha(1f).setDuration(600);
+                lmntrxText.animate().alpha(1f).setDuration(600);
+            }
+        }.start();
+        //END OF ANIMATION
+
+
+        // SHOWS SPLASH SCREEN AND HIDES IT AFTER GIVEN TIME
+
+        splashLayout.setVisibility(View.VISIBLE);
+        splashLayout.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                getSupportActionBar().show();
+                splashLayout.setVisibility(View.GONE);
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED);
+            }
+
+
+        }, 6000); // CHANGE THIS VALUE TO ADJUST SPLASH SCREEN DURATION
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.activity_webview, menu);
+        return true;
     }
 
     //To navigate within the webView
@@ -163,15 +195,26 @@ public class WebViewActivity extends AppCompatActivity {
         webview.clearCache(false);
     }
 
+    public void goHome(MenuItem item) {
+        webview.loadUrl(url);
+    }
+
+    public void showInfo(MenuItem item) {
+        //TODO Start About Activity or load about page in WebView itself
+    }
+
+    public void closeApp(MenuItem item) {
+        WebViewActivity.this.finish();
+    }
+
     class MyWebViewClient extends WebViewClient {
 
-        private UrlCache urlCache = null;
 
         public MyWebViewClient(Activity activity) {
 
             //Caching site
-            this.urlCache = new UrlCache(activity);
-            this.urlCache.register(url, domain + ".html",
+            urlCache = new UrlCache(activity);
+            urlCache.register(url, domain + ".html",
                     "text/html", "UTF-8", UrlCache.ONE_MINUTE);  //TODO Change ONE_MINUTE to 6*ONE_HOUR later
 
         }
@@ -183,20 +226,21 @@ public class WebViewActivity extends AppCompatActivity {
                 findViewById(R.id.webView).setVisibility(View.VISIBLE);
             }
 
-            if (isNetworkAvailable()){
+            if (isNetworkAvailable()) {
 
                 //Loads clicked link if network is available
                 view.loadUrl(url);
-            }else {
 
-                //Loads error message when network is not available
-                findViewById(R.id.error_msg_layout).setVisibility(View.VISIBLE);//TODO Change error_msg_layout
-                findViewById(R.id.webView).setVisibility(View.GONE);
+            } else {
+
+                //Shows error message when network is not available
+                Snackbar.make(view, "Please check your connection", Snackbar.LENGTH_LONG).show();
 
             }
 
             //Initializes progress bar to change when a link is clicked or while navigating to new page
             progressBar.setVisibility(View.VISIBLE);
+
 
             return true;
         }
@@ -204,6 +248,7 @@ public class WebViewActivity extends AppCompatActivity {
 
         @Override
         public void onPageFinished(WebView view, String url) {
+
             //Finished Loading
             System.out.println("on finish");
 
@@ -212,11 +257,14 @@ public class WebViewActivity extends AppCompatActivity {
 
             //Hiding progressBar
             progressBar.setVisibility(View.GONE);
+
+            waveLoadingView.setVisibility(View.GONE);
+
         }
 
         @Override
         public WebResourceResponse shouldInterceptRequest(WebView view, String url) { //Deprecated : WebResourceRequest instead of String (API level 21)
-            return this.urlCache.load(url);
+            return urlCache.load(url);
         }
 
         @Override
